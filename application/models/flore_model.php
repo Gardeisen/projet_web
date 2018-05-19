05b501dca6f568b06e3d964999eeb1c84572c1da
GardeisenChawaf <marine.gardeisen@free.fr>
2018-05-19 13:00:00 +0200

    essai 1 model

---
+++ application/models/flore_model.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class flore_model extends CI_Model{
+    
+    protected $table ='flore';
+    
+     public function __construct() {
+        parent::__construct();
+        
+    } 
+    
+    public function index() {
+        
+    $this->load->database('default');
+
+   return $this->db->select('*')
+                    ->from('flore')
+                    ->get()
+                    ->result();
+
+}
+
+
+    }
+     
+    
+
